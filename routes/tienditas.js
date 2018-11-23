e9dd7b8a489cdb114ed994c3afb820c884c5636a
oswaldo  martinez <oswaldomartinez@MacBook-Pro-de-oswaldo.local>
2018-11-23 11:07:17 -0600

    update and create Tienditas :smirk:

--- routes/tienditas.js
+++ routes/tienditas.js
@@ -4,15 +4,58 @@ const Tiendita = require('../models/Tiendita')
 
 //create Tienditas
 router.get('/new',(req, res)=>{
-  res.render('tienditas/form')
+  const action = '/tienditas/new'
+  res.render('tienditas/form',{action})
 })
 
+router.post('/new',(req, res)=>{
+  Tiendita.create(req.body)
+    .then(tiendita=>{
+      res.render('success',tiendita)
+    }).catch(error=>{
+      console.log(error)
+      res.render('tienditas/form',{tiendita:req.body,error})
+    })
+})
 
 //list tienditas
+router.get('/',(req, res, next)=>{
+  Tiendita.find()
+    .then(tienditas=>{
+      res.render('tienditas/list',{tienditas})
+    }).catch(e=>{
+      next(e)
+    })
+})
 
 //Tienditas detail
+router.get('/detail/:id',(req, res, next)=>{
+  const {id} = req.params
+  Tiendita.findById(id)
+    .then(tiendita=>{
+      res.render('tienditas/detail',tiendita)
+    }).catch(e=>next(e))
+})
 
 //Update Tienditas
+router.get('/update/:id',(req, res, next)=>{
+  const {id} = req.params
+  const action = `/tienditas/update/${id}`  
+  Tiendita.findById(id)
+    .then(tiendita=>{
+      res.render('tienditas/form',{tiendita, action})
+    }).catch(e=>next(e))
+})
+
+router.post('/update/:id',(req, res, next)=>{
+  const {id} = req.params
+  Tiendita.findByIdAndUpdate(id,{$set:req.body},{new:false})
+    .then(tiendita=>{
+      res.redirect(`/tienditas/detail/${id}`)
+    }).catch(error=>{
+      res.render('tienditas/form',{tiendita:req.body,error})
+    })
+})
 
 //delete Tienditas
 
